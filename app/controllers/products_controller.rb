d6777ea5b8dbca52e6e02c83a5fa3e8e2b49e8fd
“deleylagit config --global user.email “deleyla@gmail.com <Deleyla@Davids-MacBook-Pro.local>
2017-11-30 18:38:39 -0600

    Add products, Modify controller to retrieve products from database, Create route to retrieve all products and display on local host

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,23 @@
 class ProductsController < ApplicationController
+  def all_products
+    #grab all products from the database
+    products = Product.all
+    product_hashes = []
+
+    #show the products to the user
+    i = 0
+    products.length.times do
+      #take each product and convert it into a hash
+      products[i]
+      the_product = {
+        name: products[i].name,
+        price: products[i].price,
+        image: products[i].image,
+        description: products[i].description
+      }
+      product_hashes << the_product
+      i += 1
+    end
+    render json: product_hashes
+  end
 end
