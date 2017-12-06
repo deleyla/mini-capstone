5f308b87f37bdd5cc0712ce22003f5e37d0512e8
“deleylagit config --global user.email “deleyla@gmail.com <Deleyla@Davids-MacBook-Pro.local>
2017-12-05 18:43:26 -0600

    Add index to route and controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,4 +21,32 @@ class ProductsController < ApplicationController
     render json: product_hashes
   end
 
+  def show
+    # THIS IS A QUERY PARAMETER
+    # in the url: http://localhost:3000/show-the-product/?name=orange%20resin%20earrings
+    # will show the information for orange resin earrings
+    # With a query parameter, you have to input the key value in the url, it is case sensitive. i.e. /?name=orange resin earrings
+    # This is not the most practical way of doing things for the user experience
+    # get some user input from my params hash
+    the_product_name = params['name']
+    product = Product.find_by(name: the_product_name)
+    render json: product.as_json
+  end
+
+  def segment
+    # THIS IS A SEGMENT PARAMETER
+    # get some user input the key in the params hash
+    # use that info to grab something from the db
+    the_id = params['id']
+    product = Product.find_by(id: the_id)
+    render json: product.as_json
+  end
+
+  def index
+    # get all products from my db
+    products = Product.all
+    # show the user all the products in my db
+    render json: products.as_json
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get '/all_products' => 'products#all_products'
+  get '/show-the-product' => 'products#show'
+  get '/one-of-the-products/:id' => 'products#segment'
+  get '/products' => 'products#index'
 end
