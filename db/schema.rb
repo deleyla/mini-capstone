9e5dbf7beb5c62302df36d5ca1a876ffa9b3a930
“deleylagit config --global user.email “deleyla@gmail.com <Deleyla@Davids-MacBook-Pro.local>
2017-12-10 11:01:48 -0600

    Change price data type to decimal

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171210163917) do
+ActiveRecord::Schema.define(version: 20171210165553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
+    t.decimal "price", precision: 9, scale: 2
     t.string "image"
     t.text "description"
     t.datetime "created_at", null: false
