8127f063d8139449f8cdd702f1f9b701d67884a9
“deleylagit config --global user.email “deleyla@gmail.com <Deleyla@Davids-MacBook-Pro.local>
2018-01-15 23:10:04 -0600

    Add Product Creation Page

--- public/index.js
+++ public/index.js
@@ -8,83 +8,80 @@ var HomePage = {
     };
   },
   created: function() {
-    console.log('in created')
-    this.runSomething();
   },
   methods: {
     runSomething: function() {
-      console.log('running something')
     }
   },
   computed: {}
 };
 
 // // Signup Vue.js
-// var SignupPage = {
-//   template: "#signup-page",
-//   data: function() {
-//     return {
-//       name: "",
-//       email: "",
-//       password: "",
-//       passwordConfirmation: "",
-//       errors: []
-//     };
-//   },
-//   methods: {
-//     submit: function() {
-//       var params = {
-//         name: this.name,
-//         email: this.email,
-//         password: this.password,
-//         password_confirmation: this.passwordConfirmation
-//       };
-//       axios
-//         .post("/v1/users", params)
-//         .then(function(response) {
-//           router.push("/login");
-//         })
-//         .catch(
-//           function(error) {
-//             this.errors = error.response.data.errors;
-//           }.bind(this)
-//         );
-//     }
-//   }
-// };
+var SignupPage = {
+  template: "#signup-page",
+  data: function() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+        password_confirmation: this.passwordConfirmation
+      };
+      axios
+        .post("/v1/users", params)
+        .then(function(response) {
+          router.push("/login");
+        })
+        .catch(
+          function(error) {
+            this.errors = error.response.data.errors;
+          }.bind(this)
+        );
+    }
+  }
+};
 // // Login vue.js
-// var LoginPage = {
-//   template: "#login-page",
-//   data: function() {
-//     return {
-//       email: "",
-//       password: "",
-//       errors: []
-//     };
-//   },
-//   methods: {
-//     submit: function() {
-//       var params = {
-//         auth: { email: this.email, password: this.password }
-//       };
-//       axios
-//         .post("/user_token", params)
-//         .then(function(response) {
-//           axios.defaults.headers.common["Authorization"] =
-//             "Bearer " + response.data.jwt;
-//           localStorage.setItem("jwt", response.data.jwt);
-//           router.push("/");
-//         })
-//         .catch(
-//           function(error) {
-//             this.errors = ["Invalid email or password."];
-//             this.email = "";
-//             this.password = "";
-//           }.bind(this)
-//         );
-//     }
-//   }
-// };
+var LoginPage = {
+  template: "#login-page",
+  data: function() {
+    return {
+      email: "",
+      password: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        auth: { email: this.email, password: this.password }
+      };
+      axios
+        .post("/user_token", params)
+        .then(function(response) {
+          axios.defaults.headers.common["Authorization"] =
+            "Bearer " + response.data.jwt;
+          localStorage.setItem("jwt", response.data.jwt);
+          router.push("/");
+        })
+        .catch(
+          function(error) {
+            this.errors = ["Invalid email or password."];
+            this.email = "";
+            this.password = "";
+          }.bind(this)
+        );
+    }
+  }
+};
 // New Product Page vue.js
 var NewProductPage = {
   template: "#new-product-page",
@@ -92,12 +89,7 @@ var NewProductPage = {
     return {
       name: "",
       price: "",
-      tax: "",
-      total: "",
-      isDiscounted: "",
       description: "",
-      availability: "",
-      supplier: "",
       images: "",
       errors: []
     };
@@ -109,12 +101,7 @@ var NewProductPage = {
       var params = {
         name: this.name,
         price: this.price,
-        tax: this.tax,
-        total: this.total, 
-        is_discounted: this.isDiscounted,
         description: this.description,
-        availability: this.availability, 
-        supplier: this.supplier,
         images: this.images
       };
       axios
@@ -136,11 +123,11 @@ var NewProductPage = {
 
 var router = new VueRouter({
   routes: [
-     { path: "/", component: HomePage },
+    { path: "/", component: HomePage },
     // // route to signup page
-    // { path: "/signup", component: SignupPage },
+    { path: "/signup", component: SignupPage },
     // // route to login page
-    // { path: "/login", component: LoginPage },
+    { path: "/login", component: LoginPage },
     // route to create new product
     { path: "/products/new", component: NewProductPage }
   ],
